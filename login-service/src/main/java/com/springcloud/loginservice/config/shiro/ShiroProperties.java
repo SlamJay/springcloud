1b9221a2b20c0da755c5475a91f94c6d4c6933bf
lijie <lijie@g7.com.cn>
2019-08-14 15:13:22 +0800

    init shiro properties

--- login-service/src/main/java/com/springcloud/loginservice/config/shiro/ShiroProperties.java
+++ login-service/src/main/java/com/springcloud/loginservice/config/shiro/ShiroProperties.java
@@ -1,4 +1,15 @@
 package com.springcloud.loginservice.config.shiro;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "spring.shiro")
 public class ShiroProperties {
+
+    private String loginUrl;
+    private String logoutUrl;
+    private String anonUrl;
 }
